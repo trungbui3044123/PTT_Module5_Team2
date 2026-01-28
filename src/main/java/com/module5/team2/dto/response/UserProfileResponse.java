ed73715f64918f12b55935593a2e8cbdd8f7cebc
tuanquoc-dev <vuquvvuquoctuan1082k4@gmai.gmaicom>
2026-01-28 23:10:13 +0700

    update

--- src/main/java/com/module5/team2/dto/response/UserProfileResponse.java
+++ src/main/java/com/module5/team2/dto/response/UserProfileResponse.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @Data
 @Builder
-public class UserProfileResponse {
+public class    UserProfileResponse {
     private Integer id;
     private String username;
     private String email;
