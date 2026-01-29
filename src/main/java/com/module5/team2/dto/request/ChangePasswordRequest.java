449e08c9dbb4ee21e59f1f215878ef247d91a641
tuanquoc-dev <vuquvvuquoctuan1082k4@gmai.gmaicom>
2026-01-29 21:00:16 +0700

    thêm validate

--- src/main/java/com/module5/team2/dto/request/ChangePasswordRequest.java
+++ src/main/java/com/module5/team2/dto/request/ChangePasswordRequest.java
@@ -1,9 +1,19 @@
 package com.module5.team2.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class ChangePasswordRequest {
+
+    @NotBlank(message = "Mật khẩu cũ không được để trống")
     private String oldPassword;
+
+    @NotBlank(message = "Mật khẩu mới không được để trống")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
+            message = "Mật khẩu phải ≥ 8 ký tự, gồm chữ hoa, chữ thường, số và ký tự đặc biệt"
+    )
     private String newPassword;
 }
