7fc771bf12e5746932b8b96b36c62a0b79e6d744
WanBi <anhnbt.it@gmail.com>
2026-01-29 20:10:23 +0700

    - Validate password and email

--- src/main/java/com/module5/team2/dto/request/RegisterRequest.java
+++ src/main/java/com/module5/team2/dto/request/RegisterRequest.java
@@ -2,8 +2,10 @@ package com.module5.team2.dto.request;
 
 import com.module5.team2.entity.UserEntity;
 import com.module5.team2.enums.Role;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -11,8 +13,10 @@ public class RegisterRequest {
     @NotBlank(message = "Username is required")
     private String username;
     @NotBlank(message = "Password is required")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$")
     private String password;
     @NotBlank(message = "Email is required")
+    @Email
     private String email;
     private String phone;
     private String name;
