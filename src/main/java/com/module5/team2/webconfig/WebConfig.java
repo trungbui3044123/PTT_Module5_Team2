f03e600e6bf3d62a4dd96518ee8d8505b9782cdb
TQuocPhu <tqphu240804@gmail.com>
2026-02-07 21:19:34 +0700

    Config: add request fetch

--- src/main/java/com/module5/team2/webconfig/WebConfig.java
+++ src/main/java/com/module5/team2/webconfig/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("" +
+                        "http://127.0.0.1:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
@@ -33,5 +34,6 @@ public class WebConfig implements WebMvcConfigurer {
             }
         });
     }
+
 
 }
