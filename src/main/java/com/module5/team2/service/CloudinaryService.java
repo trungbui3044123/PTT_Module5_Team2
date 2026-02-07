dae0c4d84ccc9f6be3f05ef35d12574ea46fbedc
TQuocPhu <tqphu240804@gmail.com>
2026-02-07 21:30:19 +0700

    Final cloudinary

--- src/main/java/com/module5/team2/service/CloudinaryService.java
+++ src/main/java/com/module5/team2/service/CloudinaryService.java
@@ -2,6 +2,7 @@ package com.module5.team2.service;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,8 +10,9 @@ import java.io.IOException;
 import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class CloudinaryService {
-    private Cloudinary cloudinary;
+    private final Cloudinary cloudinary;
 
     public Map upload(MultipartFile file) throws IOException {
         return cloudinary.uploader().upload(file.getBytes(),
