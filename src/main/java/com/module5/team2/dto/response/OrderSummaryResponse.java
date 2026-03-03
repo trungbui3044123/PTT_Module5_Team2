f4b3bffbce8eceb850fdae45cc29cd34bd5531c9
TQuocPhu <tqphu240804@gmail.com>
2026-03-03 12:29:42 +0700

    Thêm các dto liên quan tới Order và thêm reject reason vào order Entity

---
+++ src/main/java/com/module5/team2/dto/response/OrderSummaryResponse.java
@@ -0,0 +1,20 @@
+package com.module5.team2.dto.response;
+
+import com.module5.team2.enums.OrderStatus;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class OrderSummaryResponse {
+    private Long id;
+    private OrderStatus status;
+    private String receiverName;
+    private String receiverPhone;
+    private String receiverAddress;
+    private BigDecimal totalAmount;
+    private LocalDateTime createdAt;
+}
